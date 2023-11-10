8b778550f7b6a830bd7e4f8d2479e5230eb3a4a1
okemawo <okemawo@gmail.com>
2023-11-09 21:20:50 -0500

    updates to code for mapper

--- Question-1/code_without_combiner/temperature_mapper.py
+++ Question-1/code_without_combiner/temperature_mapper.py
@@ -4,8 +4,8 @@ import sys
 
 for line in sys.stdin:
     line = line.strip()
-    temperature = int(line[87:91])
+    temperature = int(line[87:92])
     quality = line[92:93]
     year_month_day = line[15:23]
-    if quality in ['0','1','4','5','9'] and temperature != 999:
+    if quality in ['0','1','4','5','9'] and temperature != 9999:
         print("%s\t%s" % (year_month_day, temperature))
